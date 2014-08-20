058b800abbb7c6f9bccb9957a3a0a28205461ba9
globalavocado <globalavocado@gmail.com>
2014-08-20 17:27:03 +0100

    all tests passed for array of 1 to 10

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,8 +2,14 @@ class RomanNumerals
 
 		def self.convert(number)
 			return 'X' if number == 10
+			return 'IX' if number == 9
+			return 'VIII' if number == 8
+			return 'VII' if number == 7
+			return 'VI' if number == 6
 			return 'V' if number == 5
-			"I"
+			return 'IV' if number == 4
+			"I" * number
 		end 
-		
 end
+
+(1..10).each {|number| puts RomanNumerals.convert(number)}
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -6,8 +6,28 @@ describe "roman_numerals" do
 		expect(RomanNumerals.convert(1)).to eq "I"
 	end
 
+	it "replaces 4 with IV" do
+		expect(RomanNumerals.convert(4)).to eq "IV"
+	end
+
 	it "replaces 5 with V" do
 		expect(RomanNumerals.convert(5)).to eq "V"
+	end
+
+	it "replaces 6 with VII" do
+		expect(RomanNumerals.convert(6)).to eq "VI"
+	end
+
+	it "replaces 7 with VII" do
+		expect(RomanNumerals.convert(7)).to eq "VII"
+	end
+
+	it "replaces 8 with VII" do
+		expect(RomanNumerals.convert(8)).to eq "VIII"
+	end
+
+	it "replaces 9 with IX" do
+		expect(RomanNumerals.convert(9)).to eq "IX"
 	end
 
 	it 'replaces 10 with X' do
