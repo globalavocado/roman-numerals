deea1c0107841a733d346c5e3c5a1cdf722ec9cc
globalavocado <globalavocado@gmail.com>
2014-08-21 11:54:43 +0100

    all tests passed, now works for all numbers

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -3,11 +3,30 @@ class RomanNumerals
 	ROMANS = {1000=>"M", 900=>"CM", 500=>"D", 400=>"CD", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 9=>"IX", 5=>"V", 4=>"IV", 1=>"I"} 
 
 		def self.convert(number)
-			ROMANS[number]
-		end 
+			
+			#number is 4627
 
+			result = ""
+			ROMANS.each do |key, value|
+			# look in hash for first key & value pair, in this case
+			# key = 1000, so the value is M
+			
+			result << value * (number / key)
+			
+			# M (value) times 4 (which is how many times 4000 fits into 1000, the key)
+			# the result is 'MMMM', this is the first thing passed into a string
+
+			intermediate_result = (number / key)
+
+			# intermediate result is 4, as above
+
+			# 627 is: number(4627) minus the intermediate result(4) times the key(1000)
+			number -= (intermediate_result * key)
+			end 
+			result
+		end
 
 end
 
-(1..10).each {|number| puts RomanNumerals.convert(number)}
+puts RomanNumerals.convert(4627)
 
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -6,40 +6,16 @@ describe "roman_numerals" do
 		expect(RomanNumerals.convert(1)).to eq "I"
 	end
 
-	# it "replaces 4 with IV" do
-	# 	expect(RomanNumerals.convert(4)).to eq "IV"
-	# end
-
 	it "replaces 5 with V" do
 		expect(RomanNumerals.convert(5)).to eq "V"
 	end
-
-	# it "replaces 6 with VII" do
-	# 	expect(RomanNumerals.convert(6)).to eq "VI"
-	# end
-
-	# it "replaces 7 with VII" do
-	# 	expect(RomanNumerals.convert(7)).to eq "VII"
-	# end
-
-	# it "replaces 8 with VII" do
-	# 	expect(RomanNumerals.convert(8)).to eq "VIII"
-	# end
-
-	# it "replaces 9 with IX" do
-	# 	expect(RomanNumerals.convert(9)).to eq "IX"
-	# end
 
 	it 'replaces 10 with X' do
 		expect(RomanNumerals.convert(10)).to eq "X"
 	end
 
-	# it 'replaces 10 with X' do
-	# 	expect(RomanNumerals.convert(10)).to eq "X"
-	# end
-
-	# it 'replaces 2000 with MM' do
-	# 		expect(RomanNumerals.convert(2000)).to eq "MM"
-	# end
+	it 'replaces 4627 with MMMMDCXXVII' do
+			expect(RomanNumerals.convert(4627)).to eq "MMMMDCXXVII"
+	end
 
 end
